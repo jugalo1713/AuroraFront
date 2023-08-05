ccc607ac41742bcd237815bbd96e7102e01b7b89
julian-gallo-nexer <julian.gallo2@nexergroup.com>
2023-08-05 11:28:17 -0500

    update

--- src/app/Components/LoginForm.jsx
+++ src/app/Components/LoginForm.jsx
@@ -1,29 +1,62 @@
+"use client"
+
 import { TextField } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 import Button from '@mui/material/Button';
+import axios from 'axios';
 export const LoginForm = () => {
 
-const onLogin = ()=>{
-    
-}
+
+    // useEffect(() => {
+    //     axios.get('https://localhost:7152/weatherforecast')
+    //     //axios.get('https://beskar-aurora-be.azurewebsites.net/weatherforecast')
+    //     .then(function (response) {
+    //       // handle success
+    //       console.log(response);
+    //     })
+    // }, []);
+
+    const handleForm = (e) => {
+        e.preventDefault();
+
+        var user = e.target.user.value;
+        var password = e.target.password.value;
+
+        //const url = `https://localhost:7152/login?user=${user}&password=${password}`;
+        const url = `https://beskar-aurora-be.azurewebsites.net/login?user=${user}&password=${password}`;
+
+        axios.post(url)
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
 
     return (
         <div className='login-form u-center'>
-            <form className='login-form__form'>
+            <form className='login-form__form' onSubmit={(e) => { handleForm(e) }}>
                 <TextField
                     required
                     id="user"
                     label="User"
                     className='login-form__input'
+                    type='email'
+                    name='email'
                 />
                 <TextField
                     required
                     id="password"
                     label="Password"
                     className='login-form__input'
+                    type='password'
+                    name='password'
                 />
-                 <Button variant="contained">Sign In</Button>
+                <Button type='submit' variant="contained">Sign In</Button>
             </form>
         </div>
     )
 }
+
